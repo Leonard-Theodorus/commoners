11d8db4d02e4b82acb527148e28301cbe8f709a8
Ellin Anggriani <107564165+Ellinanggriani@users.noreply.github.com>
2023-01-01 16:36:53 +0700

    Add files via upload

---
+++ footer.blade.php
@@ -0,0 +1,15 @@
+<footer class="bg-sky-800 px-12 py-2 top-0 w-full flex items-center justify-evenly">
+    <div class="space x-4 p-1 flex items-center ">
+        <li>
+            <a href="#" class="text-white hover:font-bold">Contact Us</a>
+        </li>
+        <li>
+            <a href="#" class="text-white hover:font-bold">Email</a>
+            <img src="" alt="Logo-email">
+        </li>
+        <li>
+            <a href="#" class="text-white hover:font-bold">Instagram</a>
+            <img src="" alt="Logo-instagram">
+        </li>
+    </div>
+</footer>
